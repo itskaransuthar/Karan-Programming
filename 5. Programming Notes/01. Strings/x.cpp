2b138726b4d67f23a412f5e92f366e438ecf024d
itskaransuthar <itskaransuthar@gmail.com>
2024-01-20 17:14:30 +0530

    k

---
+++ 5. Programming Notes/01. Strings/x.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    string str = "Karan Jii Suthar";
+
+    str.clear();
+
+    cout << str << endl;          //
+    cout << str.length() << endl; // <strong>0</strong>
+}       
