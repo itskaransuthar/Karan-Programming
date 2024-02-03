edd79ee380327eb261af59402ac18714ba7cff11
itskaransuthar <itskaransuthar@gmail.com>
2024-02-03 19:53:52 +0530

    k

--- 6. MERN/4. JavaScript/05. Arrays/Practice Set/Question3.js
+++ 6. MERN/4. JavaScript/05. Arrays/Practice Set/Question3.js
@@ -2,3 +2,11 @@
 * Filter for numbers divisible by 10 from a given array
 */
 
+let arr = [10, 29, 30, 404, 50, 340, 43];
+
+let newArr = arr.filter((num) => {
+    if(num % 10 == 0)
+        return num;
+})
+
+console.log(newArr);
